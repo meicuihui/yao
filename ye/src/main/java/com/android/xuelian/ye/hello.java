97d62987f1c0ae927aeb894717a71682e8b62cd4
2032730677@qq.com <xlmm123@>
2018-03-08 14:09:28 +0800

    test2

---
+++ ye/src/main/java/com/android/xuelian/ye/hello.java
@@ -0,0 +1,14 @@
+package com.android.xuelian.ye;
+
+import android.util.Log;
+
+/**
+ * Created by yb on 2018/3/8.
+ * @author yb
+ */
+
+public class hello {
+    public static void say(){
+        Log.d("hello","xhh");
+    }
+}
